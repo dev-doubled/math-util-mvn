f567fc813c7c737e72a293b31f6bd3b0ae999b5b
devduyled2203 <duylvd2002@gmail.com>
2023-06-28 11:10:20 +0700

    Update DDT source

--- src/test/java/org/dd/mathutil/core/test/MathUtilityAdvancedTest.java
+++ src/test/java/org/dd/mathutil/core/test/MathUtilityAdvancedTest.java
@@ -27,7 +27,7 @@ public class MathUtilityAdvancedTest {
                 {2, 2},
                 {5, 120},
                 {4, 24},
-                {6, 720}};
+                {6, 7200}};
         return testData;
     }
 
